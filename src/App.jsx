16fcec98fa325bdd9880e2a56d72a4801107ba4c
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-03-29 17:14:29 +0100

    Setup App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const H1 = styled.h1 `
+font-size: 40px;
+font-weight:800;
+`
+
+
+function App () {
+  return <H1>Hello World</H1>;
+}
+
+export default App;
