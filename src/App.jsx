7e61af504aabee1b6b224606f723dfb59bcc7749
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-04-03 10:43:03 +0200

    Styled Components properties, debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,33 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import  Button  from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 40px;
   font-weight: 800;
   background-color: yellow;
+  color: var(--color-grey-800);
 `;
 
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 8px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`;
 
-const Input = styled.input`
-  border: 2px solid #ddd;
-  border-radius: 8px;
-  padding: 0.8rem 1.6rem;
-`;
 
 const StyleApp = styled.div`
-  background-color: orange;
+  background-color: #f15309;
   padding: 20px;
 `;
 
 function App() {
   return (
-    <StyleApp>
-      <H1>Hello World</H1>
-      <Button onClick={() => alert("Check in baby")}>Check in</Button>
-      <Button onClick={() => alert("Check in baby")}>Check in</Button>
-      <Input type="number" placeholder="Number of quests"y/>
-    </StyleApp>
+    <>
+      <GlobalStyles />
+      <StyleApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in baby")}>Check in</Button>
+        <Button onClick={() => alert("Check in baby")}>Check in</Button>
+        <Input type="number" placeholder="Number of quests" />
+      </StyleApp>
+    </>
   );
 }
 
