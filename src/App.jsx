7e444674230d0227af0b04c33a0ed9aefaf19f91
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-04-18 14:07:31 +0200

    Setting React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import { BrowserRouter,Routes, Route, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Dashboard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
@@ -10,17 +11,22 @@ import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-
-
-
-
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    }
+  },
+});
 
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client = {queryClient}>
+      <ReactQueryDevtools initialIsOpen = {false} />
+
     <GlobalStyles/>
     <BrowserRouter>
       <Routes>
@@ -48,7 +54,8 @@ function App() {
         />
       </Routes>
     </BrowserRouter>
-    </>
+    </QueryClientProvider>
+    
     );
 }
 
