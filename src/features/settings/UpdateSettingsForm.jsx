47b90578e839d519715950bbace49e37e25f796a
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-04-27 17:05:43 +0200

    Updating Settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,13 +1,16 @@
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
-import { useSettings } from './useSetting';
+import { useSettings } from './useSettings';
+import { useUpdateSetting } from './useUpdateSetting';
 import Spinner from '../../ui/Spinner';
 
 function UpdateSettingsForm() {
   const {isLoading, settings:{
     minBookingLength, maxBookingLength, maxGuestPerBooking, BreakfestPrice} = {}
   } = useSettings();
+
+  const {isUpdating, updateSettings} = useUpdateSetting();
 
   if (isLoading) return <Spinner/>
 
@@ -16,15 +19,19 @@ function UpdateSettingsForm() {
       <FormRow label='Minimum nights/booking'>
         <Input type='number' id='min-nights' defaultValue={minBookingLength} />
       </FormRow>
+
       <FormRow label='Maximum nights/booking'>
         <Input type='number' id='max-nights' defaultValue={maxBookingLength} />
       </FormRow>
+
       <FormRow label='Maximum guests/booking'>
         <Input type='number' id='max-guests' defaultValue={maxGuestPerBooking} />
       </FormRow>
+
       <FormRow label='Breakfast price'>
         <Input type='number' id='breakfast-price' defaultValue={BreakfestPrice} />
       </FormRow>
+
     </Form>
   );
 }
